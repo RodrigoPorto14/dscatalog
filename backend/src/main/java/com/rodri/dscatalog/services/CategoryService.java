a34cc68c88eb36792687077380c9cf2c9f9e7444
rodri <rodrigo.porto1403@gmail.com>
2022-11-27 18:32:31 -0300

    POST Category

--- backend/src/main/java/com/rodri/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/rodri/dscatalog/services/CategoryService.java
@@ -30,4 +30,12 @@ public class CategoryService {
 		Category obj = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new CategoryDTO(obj);
 	}
+	
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);		
+	}
 }
