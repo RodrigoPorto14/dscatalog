10b7971753f61ce8d378fd3b3bb782d5c7a3ab91
rodri <rodrigo.porto1403@gmail.com>
2022-12-30 22:52:57 -0300

    Security configuration

--- backend/src/main/java/com/rodri/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/rodri/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.rodri.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,11 +17,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"};
 	
-	private static final String[] OPERATOR = { "/products/**", "/categories/**" };
+	private static final String[] OPERATOR_ADMIN = { "/products/**", "/categories/**" };
 	
 	private static final String[] ADMIN = { "/users/**" };
 	
@@ -30,14 +36,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) http.headers().frameOptions().disable(); //H2	
+		
 		http.authorizeRequests()
 		    .antMatchers(PUBLIC).permitAll()
-			.antMatchers(HttpMethod.GET, OPERATOR).permitAll()
-			.antMatchers(OPERATOR).hasAnyRole("OPERATOR","ADMIN")
+			.antMatchers(HttpMethod.GET, OPERATOR_ADMIN).permitAll()
+			.antMatchers(OPERATOR_ADMIN).hasAnyRole("OPERATOR","ADMIN")
 			.antMatchers(ADMIN).hasRole("ADMIN")
-			.anyRequest().authenticated();
-		   
-			
+			.anyRequest().authenticated();	
 	}
 	
 }
